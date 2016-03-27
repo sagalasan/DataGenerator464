9896a452adccd03a19dd9711622aa5aab14f4eba
sagalasan <sagalasan@users.noreply.github.com>
2016-03-27 16:52:57 -0600

    Created client class

--- src/models/Client.java
+++ src/models/Client.java
@@ -5,4 +5,29 @@ package models;
  */
 public class Client
 {
+  private long clientId;
+  private String name;
+  private String address;
+
+  public Client(long clientId, String name, String address)
+  {
+    this.clientId = clientId;
+    this.name = name;
+    this.address = address;
+  }
+
+  public long getClientId()
+  {
+    return clientId;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public String getAddress()
+  {
+    return address;
+  }
 }
