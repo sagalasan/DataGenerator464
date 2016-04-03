80e52140537ef58b2ea1c22ad31bab0937830d4f
sagalasan <sagalasan@users.noreply.github.com>
2016-04-03 12:14:15 -0600

    Added writing to csv-out

--- src/Generator.java
+++ src/Generator.java
@@ -11,6 +11,7 @@ public class Generator
   private List<Model> employees;
   private List<Model> groups;
   private List<Model> items;
+  private List<Model> categories;
 
   public void generate()
   {
@@ -20,11 +21,13 @@ public class Generator
     employees = CsvUtiltiy.readEmployees();
     groups = CsvUtiltiy.readGroups();
     items = CsvUtiltiy.readItems();
+    categories = CsvUtiltiy.readCategories();
 
     CsvUtiltiy.writeCsv(clients, Client.NAME, Client.HEADER);
     CsvUtiltiy.writeCsv(employees, Employee.NAME, Employee.HEADER);
     CsvUtiltiy.writeCsv(groups, Group.NAME, Group.HEADER);
     CsvUtiltiy.writeCsv(items, Item.NAME, Item.HEADER);
+    CsvUtiltiy.writeCsv(categories, Category.NAME, Category.HEADER);
   }
 
   public static void main(String[] args)
